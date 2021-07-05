17681a53fed65f1d7b1339792d7104085915c9eb
Iwata hayato <wakuwaku.shirokuma.works@gmail.com>
2021-07-05 15:10:41 +0900

    Vueファイル全体からヘッダーとフッダーをコンポーネント化して、vueファイルとして切り出す。

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,21 @@
 <template>
   <div id="app">
-     <!--<img src="./assets/logo.png">-->
+    <Header/>
     <router-view/>
+    <Footer/>
   </div>
 </template>
 
 <script>
+// コンポーネントのインポート（ヘッダー部分・フッダー部分）
+import Header from './components/Header.vue';
+import Footer from './components/Footer.vue';
+
 export default {
-  name: 'App'
+    name: 'app',
+    components: {
+      Header: Header,
+      Footer: Footer
+    }
 }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  /*margin-top: 60px;*/
-}
-</style>
